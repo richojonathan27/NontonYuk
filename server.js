328e80e057c97a8e86f919c2177ca1b8591004cc
richojonathan27 <richojonathan27@gmail.com>
2026-02-14 02:45:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ config()
 
 const app = express()
 
-const API_URL = process.env.API_URL || 'https://captain.sapimu.au/dramabox/api/v1'
+const API_URL = process.env.API_URL || 'https://dramabox.sansekai.my.id/api/dramabox/dubindo?classify=terpopuler'
 const TOKEN = process.env.AUTH_TOKEN
 
 const ALLOWED_PATHS = ['/foryou/', '/new/', '/rank/', '/search/', '/suggest/', '/classify', '/chapters/', '/watch/']
